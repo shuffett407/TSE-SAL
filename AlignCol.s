9b354d8f5b6b3b445a728e18308d9fc1dfdc2cd4
shuffett407 <98492281+shuffett407@users.noreply.github.com>
2023-03-31 18:19:35 -0400

    Update AlignCol.s

--- AlignCol.s
+++ AlignCol.s
@@ -7,9 +7,38 @@ DataDef text_data
     "justified,$right$justified,$or$center$justified$within$its$column."
 end
 
-proc main()
-    integer i, return_file, input_file, work_file, output_file, output_col, row_count, done_processing
+proc mAlignCenter(var integer row_count, var integer col_width)
+    integer i
+    string hold[255]
 
+    for i = 1 to row_count
+        GotoLine(i)
+        BegLine()
+        hold = GetText(1,255)
+        hold = format("" : ((col_width - length(hold)) shr 1), hold)
+        lReplace("^.*$",hold,"x1")
+    endfor
+
+    col_width = LongestLineInBuffer()
+end
+
+proc mAlignRight(var integer row_count, var integer col_width)
+    integer i
+    string hold[255]
+
+    for i = 1 to row_count
+        GotoLine(i)
+        BegLine()
+        hold = GetText(1,255)
+        hold = format("" : col_width - length(hold), hold)
+        lReplace("^.*$",hold,"x1")
+    endfor
+
+    col_width = LongestLineInBuffer()
+end
+
+proc main()
+    integer i, return_file, input_file, work_file, output_file, col_width, output_col, row_count, done_processing
     return_file = GetBufferId() // Current file or line should be empty
     work_file = CreateTempBuffer()
     output_file = CreateTempBuffer()
@@ -23,18 +52,24 @@ proc main()
         GotoBufferId(work_file)
         EmptyBuffer()
         CopyBlock()
-        lReplace("\$.*$","","gnx") // Remove text following first delimiter
-        MarkColumn(1,1,row_count,LongestLineInBuffer() + 1)
+        lReplace("\$.*$","$","gnx") // Removes text following first delimiter
+        lReplace("^ ","","gnx")     // Removes leading spaces
+        lReplace(" *\$$","","gnx")  // Removes trailing spaces
+        col_width = LongestLineInBuffer()
+        // Uncomment one of the following lines to change the alignment type. Default is left justified.
+        //   mAlignCenter(row_count,col_width)
+        //   mAlignRight(row_count,col_width)
+        MarkColumn(1,1,row_count,col_width + 1)
         GotoBufferId(output_file)
         GotoColumn(output_col)
         MoveBlock()
-        output_col = LongestLineInBuffer() + 2
+        output_col = LongestLineInBuffer() + 1
         GotoBufferId(input_file)
         done_processing = True
 
         for i = 1 to row_count
             GotoLine(i)
-            
+
             if CurrChar(1) <> _AT_EOL_
                 MarkLine(i,i)
 
